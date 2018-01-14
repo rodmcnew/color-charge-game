21653c7043f5f422e03cca6f9a9fbccaf3811974
rodmcnew <rodmcnew@gmail.com>
2018-01-13 22:04:23 -0600

    before moving quarks

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ const infoTextArea = document.getElementById('info');
 const world = {
     size: [512, 512],
     forceStrength: 100,
-    quarkCount: 5
+    quarkCount: 6,
+    otherQuarksMove: false
 };
 const charges = ['red', 'green', 'blue'];
+const antiCharges = ['cyan', 'pink', 'yellow'];
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -19,55 +21,62 @@ for (let i = 0; i < world.quarkCount; i++) {
     quarks.push(
         {
             position: [getRandomInt(0, world.size[0]), getRandomInt(0, world.size[1])],
+            velocity: [0, 0],
             charge: getRandomInt(0, 2)
         },
     )
 }
 
-const player = {
-    position: [world.size[0] / 2, world.size[1] / 2],
-    velocity: [0, 0],
-    charge: 0
-};
 
 function doPhysics() {
-    let totalForceVector = [0, 0];
     quarks.forEach((quark, i) => {
-        const polarityFactor = (quark.charge === player.charge ? -1 : 1);
-        const distance = [quark.position[0] - player.position[0], quark.position[1] - player.position[1]];
-        const distanceToPlayer = Math.sqrt(Math.pow(distance[0], 2) + Math.pow(distance[1], 2));
-        const angle = Math.atan2(distance[1], distance[0]);
-        let force = world.forceStrength / Math.pow(distanceToPlayer, 2) * polarityFactor;
-        if (distanceToPlayer < 10) {
-            force = 0;
+        if (i > 0 && !world.otherQuarksMove) {
+            return; //Disable having all quarks move
         }
-        const forceVector = [force * Math.cos(angle), force * Math.sin(angle)];
-        totalForceVector[0] = totalForceVector[0] + forceVector[0];
-        totalForceVector[1] = totalForceVector[1] + forceVector[1];
+        let totalForceVector = [0, 0];
+        quarks.forEach((otherQuark, otherI) => {
+            if (i === otherI) {
+                return;
+            }
+            const polarityFactor = (quark.charge === otherQuark.charge ? 1 : -1);
+            const distance = [quark.position[0] - otherQuark.position[0], quark.position[1] - otherQuark.position[1]];
+            const totalDistance = Math.sqrt(Math.pow(distance[0], 2) + Math.pow(distance[1], 2));
+            const angle = Math.atan2(distance[1], distance[0]);
+            let force = world.forceStrength / Math.pow(totalDistance, 2) * polarityFactor;
+            if (world.otherQuarksMove) {
+                force = force / 2;
+            }
+            if (totalDistance < 10) {
+                force = 0;
+            }
+            const forceVector = [force * Math.cos(angle), force * Math.sin(angle)];
+            totalForceVector[0] = totalForceVector[0] + forceVector[0];
+            totalForceVector[1] = totalForceVector[1] + forceVector[1];
+        });
+        quark.velocity[0] += totalForceVector[0];
+        quark.velocity[1] += totalForceVector[1];
+        quark.position[0] += quark.velocity[0];
+        quark.position[1] += quark.velocity[1];
+
+        if (quark.position[0] > world.size[0] || quark.position[0] < 0) {
+            quark.velocity[0] = quark.velocity[0] * -1;
+        }
+        if (quark.position[1] > world.size[1] || quark.position[1] < 0) {
+            quark.velocity[1] = quark.velocity[1] * -1;
+
+        }
     });
-    const totalVelocity = Math.sqrt(Math.pow(player.velocity[0], 2) + Math.pow(player.velocity[1], 2));
-    player.velocity[0] += totalForceVector[0];
-    player.velocity[1] += totalForceVector[1];
-    player.position[0] += player.velocity[0];
-    player.position[1] += player.velocity[1];
-
-    if (player.position[0] > world.size[0] || player.position[0] < 0) {
-        player.velocity[0] = player.velocity[0] * -1;
-    }
-    if (player.position[1] > world.size[1] || player.position[1] < 0) {
-        player.velocity[1] = player.velocity[1] * -1;
-
-    }
+    const totalVelocity = Math.sqrt(Math.pow(quarks[0].velocity[0], 2) + Math.pow(quarks[0].velocity[1], 2));
 
     infoTextArea.value = (
         'Velocity' + ': ' + totalVelocity.toFixed(2) + '\n' +
         (totalVelocity >= 10 ? '(!) Winning (!)' : '') + '\n\n\n' +
-        'Fx' + ': ' + totalForceVector[0].toFixed(2) + '\n' +
-        'Fy' + ': ' + totalForceVector[1].toFixed(2) + '\n' +
-        'Vx' + ': ' + player.velocity[0].toFixed(2) + '\n' +
-        'Vy' + ': ' + player.velocity[1].toFixed(2) + '\n' +
-        'Px' + ': ' + player.position[0].toFixed(2) + '\n' +
-        'Py' + ': ' + player.position[1].toFixed(2) + '\n'
+        // 'Fx' + ': ' + totalForceVector[0].toFixed(2) + '\n' +
+        // 'Fy' + ': ' + totalForceVector[1].toFixed(2) + '\n' +
+        'Vx' + ': ' + quarks[0].velocity[0].toFixed(2) + '\n' +
+        'Vy' + ': ' + quarks[0].velocity[1].toFixed(2) + '\n' +
+        'Px' + ': ' + quarks[0].position[0].toFixed(2) + '\n' +
+        'Py' + ': ' + quarks[0].position[1].toFixed(2) + '\n'
     );
 
 }
@@ -82,17 +91,15 @@ function render() {
     });
 
     ctx.beginPath();
-    ctx.arc(player.position[0], player.position[1], 5, 0, 2 * Math.PI);
-    ctx.fillStyle = charges[player.charge];
-    ctx.fill();
-    ctx.arc(player.position[0], player.position[1], 5, 0, 2 * Math.PI);
+    ctx.arc(quarks[0].position[0], quarks[0].position[1], 5, 0, 2 * Math.PI);
+    // ctx.strokeStyle = antiCharges[quarks[0].charge];
     ctx.stroke();
 
     const velocityLineMultiplier = 10;
-    ctx.moveTo(player.position[0], player.position[1]);
+    ctx.moveTo(quarks[0].position[0], quarks[0].position[1]);
     ctx.lineTo(
-        player.position[0] - player.velocity[0] * velocityLineMultiplier,
-        player.position[1] - player.velocity[1] * velocityLineMultiplier
+        quarks[0].position[0] - quarks[0].velocity[0] * velocityLineMultiplier,
+        quarks[0].position[1] - quarks[0].velocity[1] * velocityLineMultiplier
     );
     ctx.stroke();
 }
@@ -105,13 +112,13 @@ setInterval(() => {
 document.body.addEventListener('keydown', (e) => {
     switch (e.key) {
         case 'a':
-            player.charge = 0;
+            quarks[0].charge = 0;
             break;
         case 's':
-            player.charge = 1;
+            quarks[0].charge = 1;
             break;
         case 'd':
-            player.charge = 2;
+            quarks[0].charge = 2;
             break;
     }
 });
